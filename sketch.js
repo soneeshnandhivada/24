020600f408d0b298358dfd56d0c4c04e94238689
soneeshnandhivada <79779458+soneeshnandhivada@users.noreply.github.com>
2021-05-22 11:14:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,13 +26,13 @@ var damage=0.5 * weight* speed* speed/(thickness *thickness *thickness);
 
 if(damage>10)
 {
-wall.shapeColor=color(255,0,0);
+bullet.shapeColor=color(255,0,0);
 
 }
 
 if(damage<10)
 {
-  wall.shapeColor=color(0,255,0);
+  bullet.shapeColor=color(0,255,0);
   
 }
   }
